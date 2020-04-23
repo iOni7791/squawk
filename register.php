4ac1a6470b5084a791f9f97c32d67e1378ffbf8f
Javier <javiermacedo@infosir.com.ar>
2020-04-23 15:01:30 -0300

    cambios en los php separo el footer

--- register.php
+++ register.php
@@ -116,53 +116,10 @@
         </div> <!-- Main-container -->
     </main>
 
-    <footer>
-        <div class="main-footer-container">
-            <nav class="barra-navegacion">
-                <ul>  
-                    <li>
-                        <a class="active fs-1-5rem border-black" href="register.php"> Registro <br>
-                            <span title="Crea una cuenta para buitrear!">
-                                <i class="fa fa-user-plus" aria-hidden="true"></i>
-                            </span>
-                        </a>
-                    </li>
-                    
-                    <li>
-                        <a class="fs-1-5rem border-black" href="login.php"> Ingreso <br>
-                            <span title="Ingresa a tu nido">
-                            <i class="fa fa-sign-in fs-1-5rem" aria-hidden="true"></i>
-                            </span>
-                        </a>
-                    </li>
-                    
-                    <li>
-                        <a class="fs-1-5rem border-black" href="index.php"> Inicio <br>
-                            <span title="De vuelta al nido">
-                                <i class="fa fa-home" aria-hidden="true"></i>
-                            </span>
-                        </a>
-                    </li>
-                    
-                    <li>
-                        <a class="fs-1-5rem border-black" href="faq.php">
-                            <span title="Preguntas Frecuentemente Graznadas"> F.A.Q. <br>
-                                <i class="fa fa-question-circle" aria-hidden="true"></i>
-                            </span>
-                        </a>
-                    </li>
-                    
-                    <li>
-                        <a class="fs-1-5rem border-black" href="contacto.php">
-                            <span title="Un contacto para graznarnos"> Contacto <br>
-                                <i class="fa fa-envelope" aria-hidden="true"></i>
-                            </span>
-                        </a>
-                    </li>
-                </ul>
-            </nav>
-        </div> <!-- main-footer-container -->         
-    </footer>
+    <?php
+        $active = 3;
+        require_once("footer.php");
+    ?>
 </body>
 
 </html>
