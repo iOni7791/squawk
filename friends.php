b42af5eb1bfa3e10a383a8e8ead7d943883b2c3b
C. J. Quispe <cjquispe991@gmail.com>
2020-05-15 04:14:25 -0300

    Le metí un toquecito más de mano
    Terminé con la maqueta de amigos, pero ahora quiero ver si puedo meter un banner con info del perfil que se visualiza. Más tarde sigo.

--- friends.php
+++ friends.php
@@ -24,14 +24,85 @@
 
     <main>
         <div class="main-container">
+
+            <section class="profile-banner-container">
+                <div class="profile-banner-info">
+                    <div class="img-banner">
+                        <a href="profile.php"><img src="img/fullstack.PNG" alt="Imagen de banner de [Usuario]"></a>
+                    </div>
+                    <div class="profile-banner-picture">
+                        <a href="posts.php"><img src="img/perfil-prueba.png" alt="Foto de perfil de [Usuario]"></a>
+                    </div>
+                    <div class="profile-banner-name center-text">
+                        <a href="profile.php">Mi Nombre de Usuario</a>
+                    </div>
+                </div>
+                <div class="profile-banner-statistics">
+                    <div class="user-posts">
+                        XXX <br> posts
+                    </div>
+                    <span class="divisor"></span>
+                    <div class="user-reactions">
+                        XXX personas <br> reaccionaron 
+                    </div>
+                    <span class="divisor"></span>
+                    <div class="user-shared-posts">
+                        XXX personas <br> compartieron posts
+                    </div>
+                </div>
+            </section>
+
             <section class="main-title-container">
                 <h1 class="text-center">
                     Amigos!
                 </h1>
             </section>
-            
+
+            <section class="main-index-container">
+                <div class="input-group">
+                    <input type="text" class="form-control" placeholder="Buscar amigos...">
+                    <div class="input-group-append">
+                        <button class="btn btn-secondary" type="button">
+                            <i class="fa fa-search"></i>
+                        </button>
+                    </div>
+                </div>
+            </section>
+
             <section class="main-index-container"> <!--Recicled because muh code-->
-               
+
+                <div class="friends-container">
+                    <div class="friends">
+                        <div class="friend-profile-picture">
+                            <a href="profile.php"><img src="img/logo.png" alt="NombreDePerfil"></a>
+                        </div>
+                        <div class="friend-username">
+                            <p><a href="profile.php">Amigo1</a></p> 
+                            <em>XXX amigos en com&uacute;n</em>
+                        </div>
+                    </div>
+
+                    <div class="friends">
+                        <div class="friend-profile-picture">
+                            <a href="profile.php"><img src="img/logo.png" alt="NombreDePerfil"></a>
+                        </div>
+                        <div class="friend-username">
+                            <p><a href="profile.php">Amigo2</a></p> 
+                            <em>XXX amigos en com&uacute;n</em>
+                        </div>
+                    </div>
+
+                    <div class="friends">
+                        <div class="friend-profile-picture">
+                            <a href="profile.php"><img src="img/logo.png" alt="NombreDePerfil"></a>
+                        </div>
+                        <div class="friend-username">
+                            <p><a href="profile.php">Amigo3</a></p> 
+                            <em>XXX amigos en com&uacute;n</em>
+                        </div>
+                    </div>
+
+                </div>
             </section> <!-- Main-index-container -->
             
         </div> <!-- Main-container -->
