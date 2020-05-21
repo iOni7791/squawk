4b358fc37cc752d4297e68101ef4965ab100db21
Javier <javiermacedo@infosir.com.ar>
2020-05-20 22:33:34 -0300

    agregue las imagenes

--- app/Http/Controllers/pages.php
+++ app/Http/Controllers/pages.php
@@ -3,33 +3,60 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use phpDocumentor\Reflection\Types\This;
 
 class pages extends Controller
 {
     //
+    protected function goPosts(){
+        $activo = 1;
+        return view('home',compact('activo'));
+    }
+
     public function index()
     {
         $activo = 1;
-        return view('home',compact('activo'));
+        if (Auth::guest()):
+            return view('home',compact('activo'));
+        else:
+            return $this->goPosts();
+        endif;
     }
     public function milogin()
     {
         $activo = 2;
-        return view('auth.login',compact('activo'));
+        if (Auth::guest()):
+            return view('auth.login',compact('activo'));
+        else:
+            return $this->goPosts();
+        endif;
     }
     public function miregister()
     {
         $activo = 3;
-        return view('auth.register',compact('activo'));
+        if (Auth::guest()):
+            return view('auth.register',compact('activo'));
+        else:
+            return $this->goPosts();
+        endif;
     }
     public function faq()
     {
         $activo = 4;
-        return view('faq',compact('activo'));
+        if (Auth::guest()):
+            return view('faq',compact('activo'));
+        else:
+            return $this->goPosts();
+        endif;
     }
     public function contact()
     {
         $activo = 5;
-        return view('contact',compact('activo'));
+        if (Auth::guest()):
+            return view('contact',compact('activo'));
+        else:
+            return $this->goPosts();
+        endif;
     }
 }
