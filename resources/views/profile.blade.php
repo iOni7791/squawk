60a05a08351fc3e69ca063f2adf9a700adb28ad3
C. J. Quispe <cjquispe991@gmail.com>
2020-05-25 20:39:00 -0300

    fix css + (in process) profile

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -3,75 +3,118 @@
 @section('contenido')
     <main>
         <div class="main-container">
+
+            <section class="profile-banner-container">
+                <div class="profile-banner-info">
+                    <div class="banner user-posts">
+                        <h2>XXX<!--variable--></h2> <br> posts <br> graznados
+                    </div>
+                    <div class="banner profile-banner-picture">
+                        <a href="profile"><img src="img/profiles/pfp-default.png" alt="$user['name']"></a> <br>
+                        <a href="profile.php">Mi Nombre de Usuario</a>
+                    </div>
+                    <div class="banner user-reactions">
+                        <h2>XXX<!--variable--></h2> <br> personas <br> reaccionaron
+                    </div>
+                </div>
+            </section>
+
             <section class="main-title-container">
                 <h1 class="text-center">
-                    Editar Perfil
+                    {{ __('Editar Perfil') }}
                 </h1>
             </section>
-            
+    
+
+
             <!-- Datos de la cuenta -->
             <section class="main-profile-container">
-                <form action="profile.php" method="POST" enctype="multipart/form-data">
+                <form action="profile" method="POST" enctype="multipart/form-data">
+                @csrf
                     <legend class="center-text">
-                        Datos de la cuenta
-                    </legend> <br>
+                        <strong> Datos de la cuenta </strong>
+                    </legend>
                     
                     <div class="pfp-container center-text">
                         <div class="profile">
                             <label for="img">
-                                Foto de perfil
+                                <b> Foto de perfil </b> <br>
+                                <img src="../img/perfil-prueba.png" alt="Tu foto de perfil"> <br>
                             </label> <br>                          
-                            <img src="../img/perfil-prueba.png" alt="Tu foto de perfil"> <br>
+                            
                             <label for="img">
                                 Subir una imagen
+                                <label class="btn btn-primary fs-1-5rem">
+                                    Buscar <input type="file" id="img" name="img" accept="image/*" style="display: none;">
+                                </label>
                             </label> <br>
-                            <label class="btn btn-primary fs-1-5rem">
-                                Buscar <input type="file" id="img" name="img" accept="image/*" style="display: none;">
-                            </label>
-                        </div>
-
-                        <div class="profile">
-                            <label for="user_name">
-                                Nombre de usuario
-                            </label> <br>
-                            <div>
-                                <input class="form-control form-control-sm fs-1-5rem" type="text" name="user_name" value="" placeholder="Tu nombre de Usuario">
-                            </div> <br>
-
-                            <label for="webpage">
-                                Página web
-                            </label> <br>
-                            <div>
-                                <input class="form-control form-control-sm fs-1-5rem" type="text" name="user_name" value="" placeholder="www.tuPágina.com">
+                                                       
+                            <div class="center-text"> 
+                                <button class="btn btn-success fs-1-5rem" type="submit">Actualizar</button>
                             </div>
                         </div>
-                    </div> <br>
 
-                    <div class="center-text"> 
-                        <button class="btn btn-primary fs-1-5rem" type="submit">Actualizar</button>
                     </div>
                 </form>
-            </section> <br> 
+            </section> 
+            <br> 
 
             <section class="main-profile-container">
-                <form action="profile.php" method="POST">
+                <form action="profile" method="POST">
+                @csrf
                     <!-- Datos personales -->
                     <fieldset class="center-text"> 
                         <div class="form-text" class="row">
-                            <legend class="center-text">Datos Personales</legend>
-                            <div>
-                                <input class="form-control form-control-sm fs-1-5rem" type="text" name="first_name" value="" placeholder="Tu nombre" required>
+                            <legend class="center-text"> <strong> Datos Personales </strong></legend>
+                      
+                            <div class="form-group row">
+                                <label for="name" class="col-md-4 col-form-label text-md-right"><strong>{{ __('Nombre') }}</strong></label>
+
+                                <div class="col-md-8">
+                                    <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+
+                                    @error('name')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                </div>
                             </div>
                             
-                            <div>
-                                <input class="form-control form-control-sm fs-1-5rem" type="text" name="last_name" value="" placeholder="Tu apellido (opcional)">
+                            <div class="form-group row">
+                                <label for="email" class="col-md-4 col-form-label text-md-right"><strong>{{ __('E-Mail') }}</strong></label>
+
+                                <div class="col-md-8">
+                                    <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
+
+                                    @error('email')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                </div>
                             </div>
                             
-                            <div>
-                                <input class="form-control form-control-sm fs-1-5rem" type="email" name="reg_email" value="" placeholder="Email" required>
+                            <div class="form-group row">
+                                <label for="email" class="col-md-4 col-form-label text-md-right"><strong>{{ __('Descríbete') }}</strong></label>
+
+                                <div class="col-md-8">
+                                    <textarea name="bio" value="{{ old('email') }}" maxlength="250" placeholder="Una pequeña descripción sobre tí..."></textarea>
+                                    <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
+
+                                    @error('bio')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                </div>
                             </div>
-                            
+
+
                             <div>
+                                <label for="user_name">
+                                    <strong>  Nombre de usuario </strong>
+                                </label> <br>    
                                 <textarea name="bio" maxlength="150" placeholder="Una pequeña descripción sobre tí..."></textarea>                            
                             </div>
                     </fieldset> <br>
@@ -112,6 +155,7 @@
             <!-- Contraseña -->
             <section class="main-profile-container">
                 <form action="profile.php" method="POST">
+                @csrf
                     <fieldset class="center-text"> 
                         <div class="form-text" class="row">
                             <legend class="center-text">Cambiar contraseña</legend>
